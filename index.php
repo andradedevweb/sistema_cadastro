1c83c57ac528ca7c50dcdb09ec3e40ee66387853
Gabriel Andrade Cunha <64712751+andradedevweb@users.noreply.github.com>
2024-03-04 14:25:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
 <?php
 
-    //configurações do servidor
-    $dbname = 'sistema_cadastro';
-    $user = 'root';
-    $password = '';
-    $host='localhost';
+    include 'database.php';
 
     //armazena as informações obtidas do formulario em variaveis
     $name = $_POST['name'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
-
-    //realiza a conecção com o banco de dados mySql
-    $pdo = new PDO("mysql:dbname=$dbname;host=$host", $user, $password);
-
-    //prepara as querys a serem chamadas 
-    $insert = $pdo->prepare("INSERT INTO `clientes` VALUES (null, ?, ?, ?)");
-    $delete = $pdo->prepare("DELETE FROM `clientes` WHERE nome=? AND email=? AND telefone=?");
-    $select = $pdo->prepare("SELECT * FROM `clientes`");
 
     //insere os valores obtidos dos formularios no banco de dados
     if(isset($_POST['submit_env'])){
