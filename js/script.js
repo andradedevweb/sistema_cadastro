40284f9412e212e3a5db668f6cc171cd8a4fd97d
Gabriel Andrade Cunha <64712751+andradedevweb@users.noreply.github.com>
2024-03-21 15:24:02 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-
+document.getElementsByName('submit_env').addEventListener("click", function(){
+            window.alert("Enviado!")
+        });
+document.getElementsByName('submit_del').addEventListener("click", function(){
+          window.alert("Deletado!")
+  });
